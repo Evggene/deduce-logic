c8d90327b67351920fa7f994912ddadb6bfcea3e
Evgeniy.Bezlepkin <evgeny-bezlepkin@mail.ru>
2019-07-09 18:32:35 +0700

    версия 1.3 (поддержка скобок, разбор с помощью state machine)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,6 @@
-import model.Model;
-import parcer.Parser;
-import parcer.ParserException;
+import model.logic.ConsoleLogic;
+import view.ConsoleView;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.*;
 
 
@@ -11,40 +8,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        if (args == null || args.length == 0) {
-            System.out.print("Missing argument");
-            return;
-        }
+        Collection<String> resultsList = new ConsoleLogic(args).makeResults();
 
-        Model model;
-
-        try {
-            model = new Parser().parse(args[0]);
-        } catch (FileNotFoundException e) {
-            System.out.print("Wrong argument: file not found");
-            return;
-        } catch (IOException e) {
-            System.out.print("Error when reading file: " + e.getMessage());
-            return;
-        } catch (ParserException e) {
-            System.out.print("Invalid file: " + e.getMessage());
-            return;
-        } catch (Exception e) {
-            System.out.print("Unknown error: " + "wrong syntax in file");
-            return;
-        }
-
-
-        Collection<String> resultsList = model.deduce();
-
-        StringBuilder sb = new StringBuilder();
-        Iterator<String> i = resultsList.iterator();
-
-        if (i.hasNext())
-            sb.append(i.next());
-        while (i.hasNext()) {
-            sb.append(", ").append(i.next());
-        }
-        System.out.print(sb);
+        new ConsoleView(resultsList).viewResults();
     }
 }
